8ec16678f6c620a2f5048b9d5e750ff462ae60c1
Konrad Pietrzynski <kpietrzy@ljzqlvv3-lon.int.openbet.com>
2023-09-25 14:38:51 +0100

    Making changes to login functionality

--- induction/training/admin/tcl/training/training .tcl
+++ induction/training/admin/tcl/training/training .tcl
@@ -1,8 +1,13 @@
 namespace eval TRAINING {
 
 	asSetAct KOJOLU_GreedyPig         [namespace code go_greedy_pig]
+	asSetAct KOJOLU_lobby         [namespace code lobby]
 
  	proc go_greedy_pig args {
   			asPlayFile -nocache training/greedy_pig/login.html
+	}
+
+	 	proc lobby args {
+  			asPlayFile -nocache training/greedy_pig/lobby.html
 	}
 }
