1e65893ff19c0a8006158c60a837c39fb2cdc17b
Jonathan9168 <77795437+Jonathan9168@users.noreply.github.com>
2023-09-25 12:50:56 +0100

    Update training .tcl

--- induction/training/admin/tcl/training/training .tcl
+++ induction/training/admin/tcl/training/training .tcl
@@ -1,8 +1,8 @@
 namespace eval TRAINING {
 
-	asSetAct KOJOLU_GreedyPig            [namespace code go_greedy_pig]
+	asSetAct KOJOLU_GreedyPig         [namespace code go_greedy_pig]
 
- 	proc go_greedy_pig {
+ 	proc go_greedy_pig args {
   			asPlayFile -nocache training/greedy_pig/login.html
 	}
 }
