ee8fe475f68c33bc30d4bda30b85adfaef62f13c
Jonathan9168 <77795437+Jonathan9168@users.noreply.github.com>
2023-09-28 17:04:10 +0100

    mainly lobby page changes

--- induction/training/admin/html/training/greedy_pig/lobby.js
+++ induction/training/admin/html/training/greedy_pig/lobby.js
@@ -1,27 +1,46 @@
 'use strict';
 
-// Get the modal
-var modal = document.getElementById("fund-modal");
+
 // Get the button that opens the modal
 var btn = document.getElementById("funds-btn");
-var span = document.getElementById("modal-close-btn");
+var okay_btn = document.getElementById("modal-close-btn");
+
 let btnDeposit = document.getElementById("deposit-btn");
 let inputDeposit = document.getElementById("amount-input");
 
-
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
-  modal.style.display = "block";
-};
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+  var action_link = "##TP_CGI_URL##?action=KOJOLU_checklimit&id=" + sessionStorage.id;
+
+  fetch(action_link)
+  .then(response => response.text())
+  .then(data => {
+    
+      console.log(data)
+      console.log(JSON.parse(data))
+      var res = JSON.parse(data)
+      var is_reached = res["is_reached"]
+      sessionStorage.setItem("remaining_limit", res["remaining_limit"])
+      
+      if  (is_reached == 1) {
+        var modal = document.getElementById("error-modal");        
+        okay_btn.addEventListener("click", function () {modal.style.display = "none";})
+      }
+      else {
+        var modal = document.getElementById("fund-modal");        
+      }
+
+      modal.style.display = "block";
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      };
+        
+  }) 
 };
 
 // btnDeposit.onclick = function() {
@@ -33,17 +52,40 @@ window.onclick = function (event) {
   
 //   fetch(action_link)
 // }
+function box_col ()
+{
+  console.log(inputDeposit.value)
+
+  if (inputDeposit.value <= 0 || inputDeposit.value > 100 || inputDeposit.value > sessionStorage.remaining_limit)
+  {
+    inputDeposit.style.background = "#db6767"  
+    
+  } 
+  else
+  {
+      inputDeposit.style.background = "white"  
+
+  }
+}
 
 async function test () {
-
-  let userid = sessionStorage.id
-  console.log("USER ID -======="+userid);
-  let amount = inputDeposit.value
-  console.log(amount)
-  let action_link = "##TP_CGI_URL##?action=KOJOLU_deposit&userid=" + userid + "&amount=" + amount;
-  console.log(action_link)
+  console.log(inputDeposit.value)
   
-  await fetch(action_link)
-  location.reload();
+  if (inputDeposit.value <= 0 || inputDeposit.value > 100 || inputDeposit.value > sessionStorage.remaining_limit)
+  {
+    inputDeposit.style.background = "#db6767"  
+    return
+  }
+
+  inputDeposit.style.background = "white"  
+
+  // let userid = sessionStorage.id
+  // console.log("USER ID ======="+userid);
+  // let amount = inputDeposit.value
+  // console.log(amount)
+  // let action_link = "##TP_CGI_URL##?action=KOJOLU_deposit&userid=" + userid + "&amount=" + amount;
+  
+  // await fetch(action_link)
+  // location.reload();
 
 }
